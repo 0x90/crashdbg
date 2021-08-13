bd59d2d33610160e2a5bc8806f1029b672fcfa2d
0x90 <oleg.kupreev@gmail.com>
2021-08-13 17:14:11 +0300

    - code cleanup and refactoring + example.cfg + version: 0.1.5

--- crashdbg/cli.py
+++ crashdbg/cli.py
@@ -20,13 +20,12 @@ def info():
     """
     Show system information
     """
+    click.secho()
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         print('running in a PyInstaller bundle')
     else:
         print('running in a normal Python process')
-
-    dbg = System.get_postmortem_debugger()
-    print("Postmorten debugger: %s" % dbg)
+    print("Postmorten debugger: %s" % System.get_postmortem_debugger())
 
 
 @cli.command()
